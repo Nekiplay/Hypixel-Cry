c403b74b510c423857a8155e2b8c191c05dcedc8
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2022-09-30 22:36:55 +0500

    HideAttack Full Done

--- src/main/java/com/example/examplemod/auto/HideAttack.java
+++ src/main/java/com/example/examplemod/auto/HideAttack.java
@@ -2,10 +2,9 @@ package com.example.examplemod.auto;
 
 import com.example.examplemod.Main;
 import com.example.examplemod.events.AttackEntity;
-import com.example.examplemod.events.RemoteServerMessage;
+import net.minecraft.network.play.client.C02PacketUseEntity;
 import net.minecraft.network.play.client.C09PacketHeldItemChange;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.world.WorldSettings;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class HideAttack {
@@ -15,17 +14,15 @@ public class HideAttack {
     {
         if (Main.configFile.HideAttack && event.attacked != null && Main.mc.thePlayer != null && Main.mc.thePlayer.inventory != null) {
             last_slot = Main.mc.thePlayer.inventory.currentItem;
-            Main.mc.getNetHandler().getNetworkManager().sendPacket(new C09PacketHeldItemChange(Main.configFile.HideAttackWeaponSlot));
-            Thread switch_delay = new Thread(() ->
-            {
-                try {
-                    Thread.sleep(0, 1);
-                    Main.mc.thePlayer.sendQueue.addToSendQueue(new C09PacketHeldItemChange(last_slot));
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            switch_delay.start();
+            event.setCanceled(true);
+            if (last_slot != Main.configFile.HideAttackWeaponSlot)
+                Main.mc.getNetHandler().getNetworkManager().sendPacket(new C09PacketHeldItemChange(Main.configFile.HideAttackWeaponSlot));
+            Main.mc.getNetHandler().getNetworkManager().sendPacket(new C02PacketUseEntity(event.attacked, C02PacketUseEntity.Action.ATTACK));
+            if (Main.mc.playerController.getCurrentGameType() != WorldSettings.GameType.SPECTATOR) {
+                Main.mc.thePlayer.attackTargetEntityWithCurrentItem(event.attacked);
+            }
+            if (last_slot != Main.configFile.HideAttackWeaponSlot)
+                Main.mc.getNetHandler().getNetworkManager().sendPacket(new C09PacketHeldItemChange(last_slot));
         }
     }
 }
