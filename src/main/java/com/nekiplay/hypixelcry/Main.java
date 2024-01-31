c59c92fc0602755e7986fa9bcdc1d7a8941b642d
Roman Danilov <35975332+Nekiplay@users.noreply.github.com>
2024-01-31 14:12:18 +0000

    Update Main.java

--- src/main/java/com/nekiplay/hypixelcry/Main.java
+++ src/main/java/com/nekiplay/hypixelcry/Main.java
@@ -42,8 +42,8 @@ public class Main
 
     public DataExtractor dataExtractor = new DataExtractor();
 
-    public static final String MODID = "hypixelcry";
-    public static final String VERSION = "2.0";
+    public static final String MODID = "hypixeladdon";
+    public static final String VERSION = "1.0.0";
 
     public static final String prefix = EnumChatFormatting.GRAY + "[" + EnumChatFormatting.GOLD + "Hypixel Cry" + EnumChatFormatting.GRAY + "] ";
     public static final String serverprefix = EnumChatFormatting.GRAY + "[" + EnumChatFormatting.YELLOW + "Remote Server" + EnumChatFormatting.GRAY + "] ";
