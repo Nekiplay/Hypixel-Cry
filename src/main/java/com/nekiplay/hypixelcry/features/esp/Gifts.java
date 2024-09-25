0dd2f8178018bb3aadb67a8a5301c9894ed7aad6
Neki_play <neci.03@mail.ru>
2024-09-25 07:10:31 +0500

    Improve Gift ESP

--- src/main/java/com/nekiplay/hypixelcry/features/esp/Gifts.java
+++ src/main/java/com/nekiplay/hypixelcry/features/esp/Gifts.java
@@ -3,12 +3,11 @@ package com.nekiplay.hypixelcry.features.esp;
 import com.nekiplay.hypixelcry.DataInterpretation.DataExtractor;
 import com.nekiplay.hypixelcry.Main;
 import com.nekiplay.hypixelcry.events.AttackEntity;
-import com.nekiplay.hypixelcry.utils.ApecUtils;
+import com.nekiplay.hypixelcry.utils.EntityUtils;
 import com.nekiplay.hypixelcry.utils.RenderUtils;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.util.BlockPos;
-import net.minecraft.util.ChatComponentText;
-import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.world.WorldEvent;
@@ -22,28 +21,6 @@ import static com.nekiplay.hypixelcry.Main.mc;
 import static com.nekiplay.hypixelcry.Main.myConfigFile;
 
 public class Gifts {
-    private static final ArrayList<BlockPos> allPositions = new ArrayList<BlockPos>(){{
-        add(new BlockPos(-1, 76, 81));
-        add(new BlockPos(-13,78,105));
-        add(new BlockPos(-24,76,97));
-        add(new BlockPos(10,76,70));
-        add(new BlockPos(-26,78,42));
-        add(new BlockPos(19,86,71));
-        add(new BlockPos(-25,76,69));
-        add(new BlockPos(-17,87,49));
-        add(new BlockPos(17,77,90));
-        add(new BlockPos(4,82,100));
-        add(new BlockPos(27,76,51));
-        add(new BlockPos(23,87,27));
-        add(new BlockPos(26,86,43));
-        add(new BlockPos(-18,79,33));
-        add(new BlockPos(-1,79,113));
-        add(new BlockPos(21,77,34));
-        add(new BlockPos(-40,76,80));
-        add(new BlockPos(-22,77,56));
-        add(new BlockPos(-18,93,20));
-        add(new BlockPos(-15,76,11));
-    }};
     private static final ArrayList<BlockPos> collected = new ArrayList<BlockPos>();
     private boolean allowRender = false;
     @SubscribeEvent
@@ -62,39 +39,19 @@ public class Gifts {
     {
         collected.clear();
     }
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void onChatMsg(ClientChatReceivedEvent event) {
-        if (event.message.getUnformattedText().contains("You have already found this Gift this year!")) {
-            double distance = 9999999;
-            BlockPos closest = null;
-            for (BlockPos pos : allPositions) {
-                double distanceCurrent = mc.thePlayer.getDistanceSq(pos);
-                if (distanceCurrent <= distance) {
-                    closest = pos;
-                    distance = distanceCurrent;
-                }
-            }
-
-            if (closest != null) {
-                if (!collected.contains(closest)) {
-                    collected.add(closest);
-                }
-            }
-        }
-    }
     @SubscribeEvent
     public void onAttackEntity(AttackEntity event)
     {
         if (event.attacked instanceof EntityArmorStand) {
             if (myConfigFile != null && myConfigFile.jerryGiftsMainPage.EnableESP && allowRender) {
 
-                for (BlockPos pos : allPositions) {
-                    double distance = event.attacked.getDistanceSq(pos);
-                    if (distance <= 4.68) {
-                        if (!collected.contains(pos)) {
-                            collected.add(pos);
+                EntityArmorStand armorStand = (EntityArmorStand) event.attacked;
+                String head = EntityUtils.getArmorStandHeadId(armorStand);
+                if (head != null && !head.isEmpty()) {
+                    if (head.equals("7732c5e4-1800-3b90-a70f-727d2969254b")) {
+                        if (!collected.contains(event.attacked.getPosition())) {
+                            collected.add(event.attacked.getPosition());
                         }
-                        break;
                     }
                 }
             }
@@ -104,11 +61,15 @@ public class Gifts {
     public void onRender(RenderWorldLastEvent event) {
         if (myConfigFile != null && myConfigFile.jerryGiftsMainPage.EnableESP && allowRender)
         {
-            for (BlockPos pos: allPositions) {
-                if (!collected.contains(pos)) {
-                    RenderUtils.drawBlockBox(pos, myConfigFile.jerryGiftsMainPage.Color.toJavaColor(), 1, event.partialTicks);
-                    if (myConfigFile.jerryGiftsMainPage.Text) {
-                        //RenderUtils.drawText("Gift", pos.getX() + 0.5, pos.getY() + 1.5, pos.getZ() + 0.5, 0.9f, myConfigFile.jerryGiftsMainPage.Color.toJavaColor(), false);
+            for (Entity entity : mc.theWorld.getLoadedEntityList()) {
+                if (entity instanceof EntityArmorStand) {
+                    EntityArmorStand armorStand = (EntityArmorStand) entity;
+                    String head = EntityUtils.getArmorStandHeadId(armorStand);
+                    if (head != null && !head.isEmpty()) {
+                        if (head.equals("7732c5e4-1800-3b90-a70f-727d2969254b") && !collected.contains(entity.getPosition())) {
+                            RenderUtils.drawBlockBox(entity.getPosition().add(0, 1, 0), myConfigFile.jerryGiftsMainPage.Color.toJavaColor(), 1, event.partialTicks);
+                            RenderUtils.renderWaypointText("Gift", entity.getPosition().add(0, 2, 0), event.partialTicks, false, myConfigFile.jerryGiftsMainPage.Color.toJavaColor());
+                        }
                     }
                 }
             }
